bcc442add5040f0b560f639485dc0a570bd1c2e1
Halil İbrahim Şener <hisener@yahoo.com>
2020-12-02 11:04:23 +0100

    Add Reactor Netty client

--- spring-webclient-connection-failure-test/src/test/java/com/github/hisener/spring/webclient/WebClientTest.java
+++ spring-webclient-connection-failure-test/src/test/java/com/github/hisener/spring/webclient/WebClientTest.java
@@ -3,15 +3,18 @@ package com.github.hisener.spring.webclient;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestInstance;
@@ -22,26 +25,44 @@ import reactor.test.StepVerifier;
 @TestInstance(PER_CLASS)
 final class WebClientTest {
     private final MockWebServer server = new MockWebServer();
-    private final CloseableHttpClient apacheClient = HttpClients.createDefault();
+    private final URI baseUrl = server.url("").uri();
+    private final HttpClient apacheClient = HttpClients.createDefault();
+
+    @BeforeAll
+    void beforeAll() {
+        StepVerifier.setDefaultTimeout(Duration.ofSeconds(5));
+    }
 
     @AfterAll
     void tearDown() throws IOException {
+        StepVerifier.resetDefaultTimeout();
         server.close();
     }
 
     @RepeatedTest(10)
     void apacheClient() throws IOException {
-        URI requestUrl = server.url("").uri();
         server.enqueue(new MockResponse());
 
-        assertThat(apacheClient.execute(new HttpGet(requestUrl)).getStatusLine().getStatusCode())
+        assertThat(apacheClient.execute(new HttpGet(baseUrl)).getStatusLine().getStatusCode())
                 .isEqualTo(HttpStatus.SC_OK);
     }
 
     @RepeatedTest(10)
-    void webClientGet() {
-        URI requestUrl = server.url("").uri();
-        WebClient client = WebClient.create(requestUrl.toString());
+    void reactorNetty() {
+        server.enqueue(new MockResponse());
+
+        reactor.netty.http.client.HttpClient.create()
+                .baseUrl(baseUrl.toString())
+                .get()
+                .response()
+                .as(StepVerifier::create)
+                .assertNext(res -> assertThat(res.status()).isEqualTo(HttpResponseStatus.OK))
+                .verifyComplete();
+    }
+
+    @RepeatedTest(10)
+    void webClient() {
+        WebClient client = WebClient.create(baseUrl.toString());
 
         server.enqueue(new MockResponse());
         client.get().retrieve().toBodilessEntity().then().as(StepVerifier::create).verifyComplete();
