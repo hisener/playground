d6d0490658069819d84b18ee9d85dfec44807f44
Halil İbrahim Şener <hisener@yahoo.com>
2020-12-02 10:14:56 +0100

    Spring WebClient round 2

--- spring-webclient-connection-failure-test/src/test/java/com/github/hisener/spring/webclient/WebClientTest.java
+++ spring-webclient-connection-failure-test/src/test/java/com/github/hisener/spring/webclient/WebClientTest.java
@@ -1,25 +1,29 @@
 package com.github.hisener.spring.webclient;
 
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
 import java.io.IOException;
 import java.net.URI;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.test.StepVerifier;
 
 @Tag("unit")
+@TestInstance(PER_CLASS)
 final class WebClientTest {
     private final MockWebServer server = new MockWebServer();
 
-    @AfterEach
+    @AfterAll
     void tearDown() throws IOException {
         server.close();
     }
 
-    @Test
+    @RepeatedTest(100)
     void clientGet() {
         URI requestUrl = server.url("").uri();
         WebClient client = WebClient.create(requestUrl.toString());
