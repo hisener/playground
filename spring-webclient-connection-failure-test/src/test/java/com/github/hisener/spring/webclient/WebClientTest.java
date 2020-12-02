9f2cc5c3137faa6fb933a0ca6ab80e5a9eab8e00
Halil İbrahim Şener <hisener@yahoo.com>
2020-12-02 10:46:08 +0100

    Add Apache HTTP client

--- spring-webclient-connection-failure-test/src/test/java/com/github/hisener/spring/webclient/WebClientTest.java
+++ spring-webclient-connection-failure-test/src/test/java/com/github/hisener/spring/webclient/WebClientTest.java
@@ -1,11 +1,16 @@
 package com.github.hisener.spring.webclient;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 import java.io.IOException;
 import java.net.URI;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
@@ -17,14 +22,24 @@ import reactor.test.StepVerifier;
 @TestInstance(PER_CLASS)
 final class WebClientTest {
     private final MockWebServer server = new MockWebServer();
+    private final CloseableHttpClient apacheClient = HttpClients.createDefault();
 
     @AfterAll
     void tearDown() throws IOException {
         server.close();
     }
 
-    @RepeatedTest(100)
-    void clientGet() {
+    @RepeatedTest(10)
+    void apacheClient() throws IOException {
+        URI requestUrl = server.url("").uri();
+        server.enqueue(new MockResponse());
+
+        assertThat(apacheClient.execute(new HttpGet(requestUrl)).getStatusLine().getStatusCode())
+                .isEqualTo(HttpStatus.SC_OK);
+    }
+
+    @RepeatedTest(10)
+    void webClientGet() {
         URI requestUrl = server.url("").uri();
         WebClient client = WebClient.create(requestUrl.toString());
 
