dee539d69d8f4cd9bec5480b08b288029da5f6da
Halil İbrahim Şener <hisener@yahoo.com>
2020-12-20 15:03:50 +0100

    Polish

--- spring-webclient-connection-failure-test/src/test/java/com/github/hisener/spring/webclient/WebClientTest.java
+++ spring-webclient-connection-failure-test/src/test/java/com/github/hisener/spring/webclient/WebClientTest.java
@@ -3,7 +3,25 @@ package com.github.hisener.spring.webclient;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.resolver.dns.DnsAddressResolverGroup;
+import io.netty.resolver.dns.DnsNameResolverBuilder;
 import java.io.IOException;
 import java.net.URI;
 import java.time.Duration;
@@ -15,12 +33,23 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.test.StepVerifier;
 
+/**
+ * Minimal reproducible example for {@code AnnotatedConnectException}s in Spring WebClient due to
+ * multiple entries in {@code /etc/hosts} file. It turned out it's netty issue.
+ *
+ * <p>See:
+ *
+ * <ul>
+ *   <li>https://github.com/reactor/reactor-netty/issues/1405
+ *   <li>https://github.com/netty/netty/issues/10834
+ */
 @Tag("unit")
 @TestInstance(PER_CLASS)
 final class WebClientTest {
@@ -33,24 +62,71 @@ final class WebClientTest {
         StepVerifier.setDefaultTimeout(Duration.ofSeconds(1));
     }
 
+    @BeforeEach
+    void setUp() {
+        server.enqueue(new MockResponse());
+    }
+
     @AfterAll
     void tearDown() throws IOException {
         StepVerifier.resetDefaultTimeout();
         server.close();
     }
 
-    @RepeatedTest(10)
+    @Test
     void apacheClient() throws IOException {
-        server.enqueue(new MockResponse());
-
         assertThat(apacheClient.execute(new HttpGet(baseUrl)).getStatusLine().getStatusCode())
                 .isEqualTo(HttpStatus.SC_OK);
     }
 
-    @RepeatedTest(10)
-    void reactorNetty() {
-        server.enqueue(new MockResponse());
+    @Test
+    void nettyClient() throws Exception {
+        NioEventLoopGroup group = new NioEventLoopGroup();
 
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+
+            bootstrap
+                    .group(group)
+                    .channel(NioSocketChannel.class)
+                    // There is no issue with DefaultAddressResolverGroup
+                    // .resolver(DefaultAddressResolverGroup.INSTANCE)
+                    .resolver(
+                            new DnsAddressResolverGroup(
+                                    new DnsNameResolverBuilder(group.next())
+                                            .channelType(NioDatagramChannel.class)))
+                    .handler(
+                            new ChannelInitializer<SocketChannel>() {
+                                @Override
+                                protected void initChannel(SocketChannel socketChannel) {
+                                    ChannelPipeline pipeline = socketChannel.pipeline();
+                                    pipeline.addLast(new HttpClientCodec());
+                                    pipeline.addLast(
+                                            new SimpleChannelInboundHandler<HttpObject>() {
+                                                @Override
+                                                protected void channelRead0(
+                                                        ChannelHandlerContext ctx, HttpObject msg) {
+                                                    if (msg instanceof LastHttpContent) {
+                                                        ctx.close();
+                                                    }
+                                                }
+                                            });
+                                }
+                            });
+
+            Channel channel =
+                    bootstrap.connect(baseUrl.getHost(), baseUrl.getPort()).sync().channel();
+            channel.writeAndFlush(
+                    new DefaultFullHttpRequest(
+                            HttpVersion.HTTP_1_1, HttpMethod.GET, baseUrl.getRawPath()));
+            channel.closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    @Test
+    void reactorNetty() {
         reactor.netty.http.client.HttpClient.create()
                 .baseUrl(baseUrl.toString())
                 .get()
@@ -60,11 +136,10 @@ final class WebClientTest {
                 .verifyComplete();
     }
 
-    @RepeatedTest(10)
+    @Test
     void webClient() {
         WebClient client = WebClient.create(baseUrl.toString());
 
-        server.enqueue(new MockResponse());
         client.get().retrieve().toBodilessEntity().then().as(StepVerifier::create).verifyComplete();
     }
 }
